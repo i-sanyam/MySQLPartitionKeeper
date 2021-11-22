539445a22d3f2e89a7fa0e3a67c7bd38b904c779
Sanyam Aggarwal <sanyam.nitj@gmail.com>
2021-11-22 13:30:30 +0530

    handle new year case and update readme

--- README.md
+++ README.md
@@ -12,8 +12,6 @@ Partition Nomenclature used -
 
 ..
 
-..
-
 x.   p_20210101 - Contains all records before 2021-01-01
 
 x+1. p_20210201 - Contains all records before 2021-02-01
@@ -24,6 +22,18 @@ x+1. p_20210201 - Contains all records before 2021-02-01
 
 n.   future     - Contains all records before MAX VALUE
 ```
+
+### Partition Offset
+
+Partition Offset are the number of partitions that have greater range than the current partition, or more formally, the partitions that have greater ordinal position than the current partition, excluding the future partition.
+
+Here are a few examples to understand about partitionOffSet - 
+
+1. Assume if the current month is k and partition for the month k+1 does not exist, then `partitionOffset` is 0. MySQLPartitionKeeper has to create a new partition for the month k+1 out of the future partition.
+
+2. Similarly, if the current month is k and partition for the month k+1 already exists, then `partitionOffset` is 1. MySQLPartitionKeeper has to create a new partition for the month k+2 out of the future partition.
+
+3. If the current month is k and partition for the month k+2 already exists, then `partitionOffset` is 2. MySQLPartitionKeeper has to create a new partition for the month k+3 out of the future partition, and so on.
 
 ## Sample Database Creation
 
@@ -40,6 +50,11 @@ CREATE TABLE `sampledb`.`tb_foo_partitioned`
 );
 ```
 
-## Note - 
-This exact nomenclature has to be used for this code to work properly.
+## Usage
+
+```bash
+NODE_ENV=sample node index.js
+```
+
+## Note -
 Ensure usage of code in a test environment, before running in production.
--- src/queries.mjs
+++ src/queries.mjs
@@ -4,17 +4,23 @@ const getMonthSerialNumber = (monthNumber) => {
   return monthNumber < 10 ? `0${monthNumber}` : monthNumber;
 }
 
-const reorganiseMonthlyPartitionQuery = (TABLE_NAME, partitionToReorganise = "future") => {
+const reorganiseMonthlyPartitionQuery = (TABLE_NAME, partitionToReorganise = "future", partitionOffset = 1) => {
   /**
    * generates query for adding a new partition 
-   * eg. In the scenario where the cronjob runs in March 2020,for adding a new partition for the month of April 2020,
-   * the new partition's name will be p_20200501 i.e. partition has all the records of less than date 2020-05-01
+   * eg. In the scenario where the cronjob runs in March 2020, for adding a new partition for the month of May 2020,
+   * the new partition's name will be p_20206001 i.e. partition has all the records of less than date 2020-06-01
    */
-  const dateObjNow = new Date();
-  const currentYear = dateObjNow.getFullYear();
-  const newPartitionMonthIdx = getMonthSerialNumber( dateObjNow.getMonth() + 1 + 1 + 1 );
-  const firstDayOfNewMonth = `${currentYear}-${newPartitionMonthIdx}-01`;
-  const newPartitionName = `p_${currentYear}${newPartitionMonthIdx}01`;
+
+  const dateObjNow          = new Date();
+  const currentYear         = dateObjNow.getFullYear();
+  const proposedMonthIdx    = dateObjNow.getMonth() + 1 + partitionOffset + 1;
+  const isNextYearPartition = (proposedMonthIdx > 12);
+
+  const newPartitionYear     = isNextYearPartition ? currentYear + 1 : currentYear;
+  const newPartitionMonthIdx = getMonthSerialNumber(isNextYearPartition ? (proposedMonthIdx - 12) : proposedMonthIdx);
+
+  const firstDayOfNewMonth = `${newPartitionYear}-${newPartitionMonthIdx}-01`;
+  const newPartitionName   = `p_${newPartitionYear}${newPartitionMonthIdx}01`;
 
   const reorgQuery = `ALTER TABLE ${TABLE_NAME} REORGANIZE PARTITION ${partitionToReorganise} INTO (` +
     `PARTITION ${newPartitionName} VALUES LESS THAN (UNIX_TIMESTAMP('${firstDayOfNewMonth}')), ` +
